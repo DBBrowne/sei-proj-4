1ad6b6a83dfd18a807128cf654f55f580ac6c739
dbbrowne <duncan.b.browne@gmail.com>
2022-05-12 12:22:53 +0100

    feat: random dummy images

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
 import React from 'react'
 import Home from './components/Home.js'
 
+// @ts-ignore
+Promise._forEach = async function (arr, fn) {
+  await arr.reduce(async function (promise, el, i) {
+    await promise
+    await fn(el, i, arr)
+  }, Promise.resolve())
+}
+
+
+
 function App() {
 
   return (
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,3 +1,4 @@
+import axios from 'axios'
 import React from 'react'
 
 import { debug } from '../config.js'
@@ -6,12 +7,29 @@ import Error from './common/Error.js'
 import Loading from './common/Loading.js'
 import EventList from './events/EventList.js'
 
+
+
+
 export default function Home (){
   const [eventsData, setEventsData] = React.useState([])
   const [isError, setIsError] = React.useState(false)
   React.useEffect(() => {
     const initEventData = async ()=>{
-      eventsApi.getAll().then(res=>{
+      eventsApi.getAll().then(async res=>{
+        // DEV: random images
+        const imageurlpromises = res.data.map(()=>{
+          const randomImageUrl = 'https://random.imagecdn.app/v1/image?width=250&height=200&format=redirect'
+          return axios.get(
+            randomImageUrl
+          ).then(res=>{
+            return res.request.responseURL
+          })
+        })
+        // @ts-ignore
+        await Promise._forEach(imageurlpromises, async (el, i)=>{
+          res.data[i].image = await el
+        })
+        // DEV END
         setEventsData(res.data)
       }).catch(err=>{
         debug && console.log(err)
